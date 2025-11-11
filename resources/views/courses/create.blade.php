379c88976db68e849650c39878778717dff35e32
Florencia Figueroa <165704067+fl0rfigueroa@users.noreply.github.com>
2025-11-11 16:19:18 -0300

    formulario
    modifique el formulario fux porque no tenia el  html

--- resources/views/courses/create.blade.php
+++ resources/views/courses/create.blade.php
@@ -1,9 +1,34 @@
-<x-app-layout : title="__('Crear curso')">
-    <form action="">
-<flux:input
-    :label="__('titulo')"
-    type="text"
-    placeholder="Ingrese el título del curso"
-/>
-</form>
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Crear Nuevo Curso') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    
+                    <form action="/courses" method="POST" class="max-w-xl mx-auto">
+                        @csrf
+                        <div class="mb-5">
+                            <label for="title" class="block mb-2 text-sm font-medium text-gray-900">Titutlo</label>
+                            <input type="text" id="title" name="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
+                        </div>
+                        <div class="mb-5">
+                            <label for="description" class="block mb-2 text-sm font-medium text-gray-900">Descripcion</label>
+                            <textarea id="description" name="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" required></textarea>
+                        </div>
+                        <div class="mb-5">
+                            <label for="price" class="block mb-2 text-sm font-medium text-gray-900">Precio</label>
+                            <input type="number" id="price" name="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
+                        </div>
+                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Enviar</button>
+                    </form>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
--- storage/framework/views/747354029356926ad55c4b8cfd1be6b98cc7cdf0.php
+++ storage/framework/views/747354029356926ad55c4b8cfd1be6b98cc7cdf0.php
@@ -6,14 +6,40 @@
 <?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag && $constructor = (new ReflectionClass(App\View\Components\AppLayout::class))->getConstructor()): ?>
 <?php $attributes = $attributes->except(collect($constructor->getParameters())->map->getName()->all()); ?>
 <?php endif; ?>
-<?php $component->withAttributes([':' => true,'title' => '__(\'Crear curso\')']); ?>
-    <form action="">
-<flux:input
-    :label="__('titulo')"
-    type="text"
-    placeholder="Ingrese el título del curso"
-/>
-</form>
+<?php $component->withAttributes([]); ?>
+     <?php $__env->slot('header', null, []); ?> 
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            <?php echo e(__('Crear Nuevo Curso')); ?>
+
+        </h2>
+     <?php $__env->endSlot(); ?>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    
+                    <form action="/courses" method="POST" class="max-w-xl mx-auto">
+                        <?php echo csrf_field(); ?>
+                        <div class="mb-5">
+                            <label for="title" class="block mb-2 text-sm font-medium text-gray-900">Titutlo</label>
+                            <input type="text" id="title" name="title" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
+                        </div>
+                        <div class="mb-5">
+                            <label for="description" class="block mb-2 text-sm font-medium text-gray-900">Descripcion</label>
+                            <textarea id="description" name="description" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500" required></textarea>
+                        </div>
+                        <div class="mb-5">
+                            <label for="price" class="block mb-2 text-sm font-medium text-gray-900">Precio</label>
+                            <input type="number" id="price" name="price" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required>
+                        </div>
+                        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Enviar</button>
+                    </form>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
  <?php echo $__env->renderComponent(); ?>
 <?php endif; ?>
 <?php if (isset($__componentOriginal8e2ce59650f81721f93fef32250174d77c3531da)): ?>
